d70370bd4db317ae903148ce871c749ec70a770e
Cecil Plaut <cecil@Cecils-MacBook-Air.local>
2022-12-29 10:51:07 -0600

    initial commit

---
+++ utils.js
@@ -0,0 +1,13 @@
+const debounce = (func, delay = 1000) => {
+    let timeoutID;
+    return (...args) => {
+      if (timeoutID) {
+        clearTimeout(timeoutID); 
+      }
+      timeoutID = setTimeout(() => {
+        func.apply(null, args);
+      }, delay); 
+    };
+  };
+
+  
